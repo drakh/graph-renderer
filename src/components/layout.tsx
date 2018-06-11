2578d9ad8bbf4242e5c7db9788b383425d0b8f14
Lubomir Panak <lubopanak@gmail.com>
2018-06-11 17:10:23 +0200

    css link

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -16,7 +16,7 @@ export class Layout extends React.Component<Props, State> {
                 <title>Graphs</title>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-                <link rel="stylesheet" href="/static/main.css"/>
+                <link rel="stylesheet" href="/static/css/main.css"/>
             </head>
             <body>
             <main id={mountPoints.APP} className="w"/>
