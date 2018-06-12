c65e743f50de55e87be0a8ebe0b84501ccb5d50d
Lubomir Panak <lubopanak@gmail.com>
2018-06-12 15:07:03 +0200

    - move test data to its own file

---
+++ src/data/data.ts
@@ -0,0 +1,76 @@
+export const nodes = [
+    {id: 'a'},
+    {id: 'b'},
+    {id: 'c'},
+    {id: 'd'},
+    {id: 'e'},
+    {id: 'f'},
+    {id: 'i'},
+    {id: 'j'},
+    {id: 'k'},
+    {id: 'l'},
+    {id: 'm'},
+    {id: 'n'},
+];
+export const edges = [
+    {
+        from: 'a',
+        to: 'a',
+    },
+    {
+        from: 'a',
+        to: 'b',
+    },
+    {
+        from: 'a',
+        to: 'c',
+    },
+    {
+        from: 'c',
+        to: 'd',
+    },
+    {
+        from: 'c',
+        to: 'l',
+    },
+    {
+        from: 'c',
+        to: 'm',
+    },
+    {
+        from: 'c',
+        to: 'n',
+    },
+    {
+        from: 'd',
+        to: 'e',
+    },
+    {
+        from: 'd',
+        to: 'd',
+    },
+    {
+        from: 'd',
+        to: 'i',
+    },
+    {
+        from: 'd',
+        to: 'j',
+    },
+    {
+        from: 'd',
+        to: 'k',
+    },
+    {
+        from: 'k',
+        to: 'c',
+    },
+    {
+        from: 'k',
+        to: 'd',
+    },
+    {
+        from: 'a',
+        to: 'f',
+    },
+];
