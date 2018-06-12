01717ae2777b20a0557f98a42414a6e178f0f7b8
Lubomir Panak <lubopanak@gmail.com>
2018-06-12 18:55:30 +0200

    - remove unused

--- src/components/graph/client/api.ts
+++ src/components/graph/client/api.ts
@@ -1,9 +1,8 @@
 import axios, { AxiosResponse } from 'axios';
-import { GraphData } from '../../../common/types';
 
 export const api = {
     data: {
-        async load(): Promise<AxiosResponse<GraphData>> {
+        async load(): Promise<AxiosResponse<any>> {
             return axios.get('/api/graph');
         },
     },
--- src/server/api/routes.ts
+++ src/server/api/routes.ts
@@ -1,22 +1,8 @@
-import * as fs from 'fs-extra';
 import { wrap } from 'async-middleware';
 import { NextFunction, Request, Response, Router } from 'express';
-import { baseDataDir } from 'common/constants';
-import { GraphData, NodeItem, Transaction } from 'common/types';
 
-const numNodes = 5000;
-const maxTrans = 1000;
-
-async function loadData(): Promise<GraphData> {
-    const tFileName = `${baseDataDir}/data-transaction-${numNodes}-${maxTrans}.json`;
-    const nFileName = `${baseDataDir}/data-nodes-${numNodes}-${maxTrans}.json`;
-    const nodes: NodeItem[] = await fs.readJson(nFileName);
-    const transactions: Transaction[] = await fs.readJson(tFileName);
-    const graph: GraphData = {
-        nodes: nodes,
-        transactions: transactions,
-    };
-    return graph;
+async function loadData(): Promise<any> {
+    // here be dragons
 }
 
 export async function serveRawData(_req: Request, res: Response, _next: NextFunction) {
