3f1e750588e30cbf8cda9bbea5ab7cef27aa9c33
Lubomir Panak <lubopanak@gmail.com>
2018-06-12 18:41:01 +0200

    pass data to graph from parent

--- src/components/graph/app.tsx
+++ src/components/graph/app.tsx
@@ -2,10 +2,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { mountPoints } from '../../common/mount-points';
 import { Graph } from './graph';
+import { nodes, edges } from 'data/data';
 
 const appEntryPoint = document.getElementById(mountPoints.APP);
 if (appEntryPoint) {
     ReactDOM.render((<Graph
         container={appEntryPoint}
+        nodes={nodes}
+        edges={edges}
     />), appEntryPoint);
 }
